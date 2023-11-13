f3afd6ce59c3750ac305fa7ecc16cc45558410c0
DNAVZM <andjelapopov1@gmail.com>
2023-11-12 16:32:09 -0800

    Tag in Cukes changed into "@wip"

--- src/test/java/com/B30G03/runners/CukesRunner.java
+++ src/test/java/com/B30G03/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         glue = "com/B30G03/step_definitions",
         dryRun = false,
 
-        tags = "", //don't forget to change it to @wip before pushing to github,
+        tags = "@wip", //don't forget to change it to @wip before pushing to github,
         //this way the tag doesn't interact with any Scenario when someone uses it after updating the project
 
 
